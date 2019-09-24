14ebe40f4a6eb85d3f231dfd75fcd42c079e4798
Kasim Ahmic <kasim.ahmic@gmail.com>
2019-09-24 00:57:02 -0400

    HOTFIX: Remove hardcoded testing URL

--- octoprint_complicationpro/static/js/geturl.js
+++ octoprint_complicationpro/static/js/geturl.js
@@ -1,6 +1,6 @@
 $.ajax({
     type: "POST",
-    url: "http://0.0.0.0:5000/api/plugin/complicationpro",
+    url: window.location + "api/plugin/complicationpro",
     data: JSON.stringify({command:"getURL",url:window.location}),
  	contentType: "application/json"
 });
